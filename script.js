8afa08c07787a229727f449e093ce8ba17ee4953
Dorina24 <euseidm@gmail.com>
2024-03-13 19:11:54 +0200

    functions

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-console.log(`Hello!`);
+// functii simple
+
+function printDigits() {
+    for (let i = 0; i <= 9; i++) {
+        console.log('digit: ', i);
+    }
+}
+
+printDigits();
+printDigits();
+printDigits();
+
+// functii cu parametri 
+function arrayHasElement(arrayToCheck, element)                   //verificam daca un array are un element
+{
+    let found=false;
+    for (const element1 of arrayToCheck)
+    { if (element1 === element)
+            {   
+            found=true;
+            break;  
+            }
+    }
+
+    if(found)
+    {console.log(`Array ${arrayToCheck} has element ${element}`);
+        }  else {
+            console.log(`Array ${arrayToCheck} does not has element ${element}`);
+        }
+}
+const array1 = [1,2,3,4,5];
+const array2 = [3, 20, 21];
+const valueToCheck1 = 10;
+const valueToCheck2 = 30;
+arrayHasElement(array1, valueToCheck1);
+arrayHasElement(array2, valueToCheck1);
+arrayHasElement(array1,valueToCheck2);
+arrayHasElement(['ala', 'bala', 'portocala'], 'ala');          //alta forma de scriere, nu trebuie intotdeauna ca parametrii sa fie variabile
+
+
+//
